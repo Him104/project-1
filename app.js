9224451a12f7e59fd81315a5edabcecdadec8bab
Subhash Mourya <subhash@euronics.com>
2023-09-10 09:46:14 +0530

    Added Bloging Site API

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+const route = require('./routes/route.js');
+const mongoose = require("mongoose");
+require('dotenv').config();
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+mongoose.connect(process.env.mongo_uri)
+
+.then( () => console.log("Mongo is connected"))
+
+.catch(err => console.log(err))
+
+app.use('/', route); 
+
+
+app.listen(process.env.PORT || 8000, function () {
+
+    console.log(`Blogging Server running on port ` + (process.env.PORT || 8000 ));
+    
+})
---
+++ models/blogModel.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const blogSchema = new mongoose.Schema({
+
+    title: {type: String,required: true,trim: true},
+    body: {type: String,required: true,trim: true},
+    authorId: {type: String,required: true,trim: true},
+    tags: {type: [String]},
+    category: {type: String, required: true,trim: true},
+    subcategory: {type: [String]},
+    isPublished: {type:Boolean,default:true},
+    isDeleted: {type:Boolean,default:false},
+    publishedAt: { type: Date, default: Date.now },
+
+     
+    
+    }
+    , { timestamps: true }); 
+    
+    module.exports = mongoose.model('blogs', blogSchema);
